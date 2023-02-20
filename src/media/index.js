a056464300a5ed7b9a597611596b124d1024165d
Patryk Orsztynowicz <patrykorsztynowicz1997@gmail.com>
2023-02-20 21:11:50 +1000

    Uploaded SVG files

---
+++ src/media/index.js
@@ -0,0 +1,13 @@
+const js = require('./js.svg')
+const css = require('./css.svg')
+const html = require('./html.svg')
+const react = require('./react.svg')
+const node = require('./node.svg')
+
+module.exports = {
+  js,
+  css,
+  html,
+  react,
+  node
+}
